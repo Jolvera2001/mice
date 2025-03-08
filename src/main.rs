f6f86790103ac0fd470469edc9b6a3616fbe4adb
Jolvera2001 <jolvera6@stedwards.edu>
2025-03-08 00:13:42 -0600

    started over with iced instead

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
-slint::include_modules!();
-fn main() -> Result<(), slint::PlatformError> {
-    let window = MainWindow::new()?;
-    window.run()?;
-    Ok(())
+use iced::widget::{button, column, text, Column};
+
+fn main() -> iced::Result {
+    iced::run("Mice", Counter::update, Counter::view)
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Message {
+    Increment,
+    Decrement
+}
+
+#[derive(Default)]
+struct Counter {
+    value: i64
+}
+
+impl Counter {
+    pub fn view(&self) -> Column<Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("-").on_press(Message::Decrement),
+        ]
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {
+            Message::Decrement => self.value -= 1,
+            Message::Increment => self.value += 1
+        }
+    }
 }
