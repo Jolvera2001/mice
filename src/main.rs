00f0fbad8532eb6864f36ba239b531f57b4592cf
Jolvera2001 <jolvera6@stedwards.edu>
2025-03-09 13:07:36 -0500

    small todo for practice

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,78 @@
-use iced::widget::{button, column, text, Column};
+use iced::widget::{button, column, row, text, text_editor, text_input, Column};
 
 fn main() -> iced::Result {
-    iced::run("Mice", Counter::update, Counter::view)
+    iced::run("Mice", App::update, App::view)
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 enum Message {
-    Increment,
-    Decrement
+    Add,
+    Delete(usize),
+    ContentChanged(String)
 }
 
 #[derive(Default)]
-struct Counter {
-    value: i64
+struct App {
+    list: Vec<String>,
+    content: String,
+    warning: String,
 }
 
-impl Counter {
+impl App {
     pub fn view(&self) -> Column<Message> {
+        let mut todo_items = Vec::new();
+
+        for (index, item) in self.list.iter().enumerate() {
+            todo_items.push(
+                row![
+                    button("-").on_press(Message::Delete(index)),
+                    text(item).size(16),
+                ]
+                .spacing(6)
+                .into()
+            );
+        }
+
+        let input_row = row![
+            text_input("Type here...", &self.content)
+                .on_input(Message::ContentChanged),
+            button("Add").on_press(Message::Add)
+        ].spacing(10);
+
+        let warning_text = if !self.warning.is_empty() {
+            text(&self.warning)
+        } else {
+            text("")
+        };
+
         column![
-            button("+").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("-").on_press(Message::Decrement),
+            column(todo_items).spacing(5),
+            input_row,
+            warning_text
         ]
+        .spacing(20)
+        .padding(20)
+        .into()
+
     }
 
     pub fn update(&mut self, message: Message) {
         match message {
-            Message::Decrement => self.value -= 1,
-            Message::Increment => self.value += 1
+            Message::ContentChanged(content) => {
+                self.content = content
+            },
+            Message::Add =>  {
+                if !self.content.is_empty() && !self.content.trim().is_empty() {
+                    self.warning.clear();
+                    self.list.push(self.content.clone());
+                    self.content.clear();
+                } else {
+                    self.warning = String::from("Cannot add an empty task");
+                }
+            },
+            Message::Delete(location) => {
+                self.list.remove(location);
+            }
         }
     }
 }
